1fa1e8c20e4b873df5b4fe2d1ff049711c0183c2
Siwoo Jeon <me@migan.co.kr>
2025-11-30 22:25:45 +0900

    refactor: registering modules

--- commands/ping.go
+++ commands/ping.go
@@ -5,40 +5,38 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-var PingCommand = &Command{
-	ApplicationCommand: &discordgo.ApplicationCommand{
-		Name:        "ping",
-		Description: "Check the bot's latency",
-	},
-	Run: func(inter *builders.InteractionCreate) error {
-		title := "### 🏓 Pong!"
-
-		if err := builders.NewMessageSender(inter).
-			AddComponents(builders.ContainerBuilder().AddText(title).AddText("- Calculating latency...")).
-			SetComponentsV2(true).
-			Send(); err != nil {
-			return err
-		}
-
-		message, err := inter.FetchReply()
-		if err != nil {
-			return err
-		}
-
-		createdTimestamp, _ := discordgo.SnowflakeTimestamp(inter.ID)
-		discordPing := message.Timestamp.Sub(createdTimestamp).Milliseconds()
-
-		return builders.NewMessageSender(inter).
-			AddComponents(
-				builders.ContainerBuilder().
-					AddText(title).
-					AddText("- **Discord latency:** `%d`ms", discordPing),
-			).
-			SetComponentsV2(true).
-			Send()
-	},
-}
-
 func init() {
-	GetDiscommand().LoadCommand(PingCommand)
+	GetDiscommand().LoadCommand(&Command{
+		ApplicationCommand: &discordgo.ApplicationCommand{
+			Name:        "ping",
+			Description: "Check the bot's latency",
+		},
+		Run: func(inter *builders.InteractionCreate) error {
+			title := "### 🏓 Pong!"
+
+			if err := builders.NewMessageSender(inter).
+				AddComponents(builders.ContainerBuilder().AddText(title).AddText("- Calculating latency...")).
+				SetComponentsV2(true).
+				Send(); err != nil {
+				return err
+			}
+
+			message, err := inter.FetchReply()
+			if err != nil {
+				return err
+			}
+
+			createdTimestamp, _ := discordgo.SnowflakeTimestamp(inter.ID)
+			discordPing := message.Timestamp.Sub(createdTimestamp).Milliseconds()
+
+			return builders.NewMessageSender(inter).
+				AddComponents(
+					builders.ContainerBuilder().
+						AddText(title).
+						AddText("- **Discord latency:** `%d`ms", discordPing),
+				).
+				SetComponentsV2(true).
+				Send()
+		},
+	})
 }
--- modals/pagination_container.go
+++ modals/pagination_container.go
@@ -10,57 +10,55 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-var PaginationContainerModal *commands.Modal = &commands.Modal{
-	Parse: func(inter *builders.InteractionCreate) bool {
-		data := inter.ModalSubmitData()
-		customID := data.CustomID
-
-		if data.Components[0].Type() != discordgo.ActionsRowComponent {
-			return false
-		}
-
-		if !strings.HasPrefix(customID, utils.PaginationEmbedModal) {
-			return false
-		}
-
-		id := utils.GetPaginationEmbedID(customID)
-		userID := utils.GetPaginationEmbedUserID(id)
-
-		if inter.Member.User.ID != userID {
-			return false
-		}
-
-		if builders.GetPaginationContainer(id) == nil {
-			return false
-		}
-
-		cmp := data.Components[0].(*discordgo.ActionsRow).Components[0].(*discordgo.TextInput)
-
-		if _, err := strconv.Atoi(cmp.Value); err != nil {
-			inter.Reply(&discordgo.InteractionResponseData{
-				Components: []discordgo.MessageComponent{
-					builders.MakeErrorContainer("The value must be number."),
-				},
-				Flags: discordgo.MessageFlagsEphemeral | discordgo.MessageFlagsIsComponentsV2,
-			})
-			return false
-		}
-
-		return true
-	},
-	Run: func(inter *builders.InteractionCreate) error {
-		data := inter.ModalSubmitData()
-		customID := data.CustomID
-		id := utils.GetPaginationEmbedID(customID)
-		p := builders.GetPaginationContainer(id)
-		cmp := data.Components[0].(*discordgo.ActionsRow).Components[0].(*discordgo.TextInput)
-
-		page, _ := strconv.Atoi(cmp.Value)
-
-		return p.Set(inter, page)
-	},
-}
-
 func init() {
-	commands.GetDiscommand().LoadModal(PaginationContainerModal)
+	commands.GetDiscommand().LoadModal(&commands.Modal{
+		Parse: func(inter *builders.InteractionCreate) bool {
+			data := inter.ModalSubmitData()
+			customID := data.CustomID
+
+			if data.Components[0].Type() != discordgo.ActionsRowComponent {
+				return false
+			}
+
+			if !strings.HasPrefix(customID, utils.PaginationEmbedModal) {
+				return false
+			}
+
+			id := utils.GetPaginationEmbedID(customID)
+			userID := utils.GetPaginationEmbedUserID(id)
+
+			if inter.Member.User.ID != userID {
+				return false
+			}
+
+			if builders.GetPaginationContainer(id) == nil {
+				return false
+			}
+
+			cmp := data.Components[0].(*discordgo.ActionsRow).Components[0].(*discordgo.TextInput)
+
+			if _, err := strconv.Atoi(cmp.Value); err != nil {
+				inter.Reply(&discordgo.InteractionResponseData{
+					Components: []discordgo.MessageComponent{
+						builders.MakeErrorContainer("The value must be number."),
+					},
+					Flags: discordgo.MessageFlagsEphemeral | discordgo.MessageFlagsIsComponentsV2,
+				})
+				return false
+			}
+
+			return true
+		},
+		Run: func(inter *builders.InteractionCreate) error {
+			data := inter.ModalSubmitData()
+			customID := data.CustomID
+			id := utils.GetPaginationEmbedID(customID)
+			p := builders.GetPaginationContainer(id)
+			cmp := data.Components[0].(*discordgo.ActionsRow).Components[0].(*discordgo.TextInput)
+
+			page, _ := strconv.Atoi(cmp.Value)
+
+			return p.Set(inter, page)
+		},
+	})
 }
